a54bacd7c18acf35ba05a1efa835b69077c55c3c
Monique Silva <moniquesntsilva@gmail.com>
2022-12-12 23:53:49 -0300

    feat: add more rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,16 @@ module.exports = {
   theme: {
     extend: {
       backgroundColor: {
-        'light-blue': 'rgba(206, 226, 233)',
+        'light-blue': 'rgb(206, 226, 233)',
         'gray-blue': 'rgb(49, 58, 73)',
         'dark-blue': 'rgb(32, 38, 50)',
+      },
+      colors: {
+        'light-blue': 'rgb(206, 226, 233)',
+        'light-green': 'rgb(83, 255, 171)',
+      },
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
       }
     },
   },
