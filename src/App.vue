ccd4e240f10a6fd07d6b7d528c63a2d648f9a9e1
Monique Silva <moniquesntsilva@gmail.com>
2022-12-17 21:36:51 -0300

    feat: update layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <main class="flex items-center justify-center h-screen w-full bg-dark-blue">
+  <main class="flex flex-col items-center justify-center gap-y-5 h-screen w-full bg-dark-blue">
     <AdviceCard />
+    <AdviceFooter />
   </main>
 </template>
 
 <script>
 import AdviceCard from './components/AdviceCard.vue'
+import AdviceFooter from './components/AdviceFooter.vue'
+
 export default {
   name: 'App',
   components: {
     AdviceCard,
+    AdviceFooter,
   },
 }
 </script>
