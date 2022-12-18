be17b8f635f45ef98bea93dbe3429d68e981cdc9
Monique Silva <moniquesntsilva@gmail.com>
2022-12-17 22:04:49 -0300

    sonarcloud

--- src/components/AdviceCard.vue
+++ src/components/AdviceCard.vue
@@ -3,7 +3,7 @@
       <h1 class="text-neon-green uppercase tracking-[0.4em] text-xs font-extrabold">Advice # </h1>
       <p class="text-center text-light-blue font-extrabold tracking-[0.02em] leading-9 text-[28px]">"It is easy to sit up and take notice, what’s difficult is getting up and taking action."</p>
       <img src="/assets/pattern-divider-desktop.svg" alt="Pattern">
-      <button class="absolute -bottom-8 left-2/4 -translate-x-2/4 w-16 h-16 flex items-center justify-center cursor-pointer bg-neon-green rounded-full hover:shadow-[0_0_32px_rgba(83,255,171,1)] hover:shadow-neon-green transition">
+      <button class="absolute -bottom-8 left-2/4 -translate-x-2/4 w-16 h-16 flex items-center justify-center cursor-pointer bg-neon-green rounded-full hover:shadow-[0_0_32px_rgba(83,255,171,1)] hover:shadow-neon-green transition duration-300">
         <img src="/assets/icon-dice.svg" alt="Dice">
       </button>
   </article>
